9e6265348bf76ca332ef2f9163833409a738a490
yangshasha05 <yangshasha05@meituan.com>
2023-08-13 14:14:02 +0800

    配置修复

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,35 +1,38 @@
-import React from "react";
-import { PureComponent, ReactNode } from "react";
+import React, { PureComponent, ReactNode } from "react";
 import HomePage from "./homPage.tsx";
-import { BrowserRouter as Router, Route, Link,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import { connect } from "react-redux";
 import { initHomePage } from "../store/actions/homePage.ts";
 
+interface Props {
+  initHomePage: () => void;
+}
 
-interface Props{
-    initHomePage:()=>void;
+class App extends PureComponent<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.props.initHomePage()
+  }
+
+  render(): ReactNode {
+    return (
+      <Router>
+        <div>
+          <HomePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    )
+  }
 }
-export class App extends PureComponent<Props>{
-    constructor(props){
-        super(props);
-        this.props.initHomePage()
-    }
-    render(): ReactNode {
-        return(
-            <Router>
-                <div>
-                    <HomePage/>
-                    <Routes>
-                        <Route  path="/" component={HomePage} />
-                        <Route path="/about" component={HomePage} />
-                    </Routes>
-                </div>
-            </Router>)
-    }
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    initHomePage: () => dispatch(initHomePage())
+  }
 }
-const mapDispatchToProps=(dispatch)=>{
-    return{
-        initHomePage:()=>dispatch(initHomePage())
-    }
-}
-export default connect(null,mapDispatchToProps)(App)
+
+export default connect(null, mapDispatchToProps)(App);
