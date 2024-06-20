da24715629d914ddb14f2fc7d615bac4bbb71543
infinity-young <110441291+infinity-young@users.noreply.github.com>
2024-06-20 21:47:37 +0800

    工程迁移适配

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,19 @@
-import { createStore,applyMiddleware,compose } from 'redux'
-import reducers from './reducers/index.ts'
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers/index.ts';
 import createSagaMiddleware from 'redux-saga';
-import saga from './saga/index.ts'
+import saga from './saga/index.ts';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-
 
 export default function createStoreByRootTag() {
   const sagaMiddleware = createSagaMiddleware();
-  const enhancer = compose(
-    applyMiddleware(sagaMiddleware),
-    composeWithDevTools()
+  
+  // 使用 composeWithDevTools 来组合中间件和 dev tools
+  const enhancer = composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+    // 可以在这里添加其他的中间件
   );
-  const store = createStore(reducers,enhancer);
+
+  const store = createStore(reducers, enhancer);
   sagaMiddleware.run(saga);
-  return store
+  return store;
 }
