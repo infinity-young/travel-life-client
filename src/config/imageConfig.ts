1018151dbd82c0d137f71aeb00e4b3858033dad0
infinity-young <110441291+infinity-young@users.noreply.github.com>
2025-10-11 00:25:39 +0800

    feat: 修正后端路径

--- src/config/imageConfig.ts
+++ src/config/imageConfig.ts
@@ -1,1 +1,2 @@
-export const IMAGE_PATH='http://localhost:8080/basic-webapp/frontend/'
+const isProd = process.env.NODE_ENV === 'production';
+export const IMAGE_PATH = isProd ? '/basic-webapp/frontend/' : 'http://localhost:8080/frontend/';
--- src/config/requestConfig.ts
+++ src/config/requestConfig.ts
@@ -1,7 +1,9 @@
-export const BATH_PATH='http://localhost:8080/basic-webapp'
-export const HOME_PAGE_PATH='/frontend/listmainpageinfo'
-export const SHOP_LIST_PAGE_FILTER_PATH='/frontend/listshopspageinfo'
-export const SHOP_LIST_PAGE_LIST_PATH='/frontend/listshops'
+
+const isProd = process.env.NODE_ENV === 'production';
+export const BATH_PATH = isProd ? '/basic-webapp' : 'http://localhost:8080';
+export const HOME_PAGE_PATH = '/frontend/listmainpageinfo';
+export const SHOP_LIST_PAGE_FILTER_PATH = '/frontend/listshopspageinfo';
+export const SHOP_LIST_PAGE_LIST_PATH = '/frontend/listshops';
 export const SHOP_DETAIL_PAGE_INFO_PATH='/frontend/listshopdetailpageinfo'
 export const SHOP_PRODUCT_PATH='/frontend/listproductsbyshop'
 export const GOODS_DETAIL_PATH='/frontend/listproductdetailpageinfo'
